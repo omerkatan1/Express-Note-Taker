a7b9ff58c74e92a06deec82f0bca09c83a2c5858
Omer Katan <omerkatan@live.com>
2019-12-02 03:02:14 -0800

    added nodemon

--- public/js/server.js
+++ public/js/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 7000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('../../public'));
 
 
 
-app.listen(7000);
+app.listen(5000);
 
