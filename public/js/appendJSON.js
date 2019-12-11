438e534a8569542bd598b2065e967e8ef9b55d3b
Omer Katan <omerkatan@live.com>
2019-12-10 19:30:42 -0800

    note

--- public/js/appendJSON.js
+++ public/js/appendJSON.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
 
-function createJSON() {
-    fs.writeFile('savedNotes.json', JSON.stringify('test', null, '\t'), function(err) {
-        if (err) throw err;
-        console.log('success! created saved notes JSON file!');
-    });
-}
+// write file
+fs.writeFile('savedNotes.json', function(err) {
+    if (err) throw err;
+    console.log('success! created saved user note file!');
+});
+
+// append file
+fs.appendFile('savedNotes.json', JSON.stringify(savedNote, null, '\t'), function(err) {
+    if (err) throw err;
+    console.log('success! appended note!');
+});
--- public/js/server.js
+++ public/js/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
+
+
+
+
 app.use(express.static('../../public'));
+
 
 app.listen(PORT);
 
