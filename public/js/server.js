2ae9cf0256b4706f431e1c41413d45cefc0300e1
Omer Katan <omerkatan@live.com>
2019-12-10 20:09:28 -0800

    note

--- public/js/server.js
+++ public/js/server.js
@@ -5,8 +5,24 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
+app.get("/api/savedNotes", function (req, res) {
+    // TODO: retrieve reservations and send them back as JSON
+    console.log(JSON.stringify(res.json));
+});
+
+app.post("/api/savedNotes", function (req, res) {
+    // TODO: receive data and create reservation
+    // res.json(req.body);
+    fs.appendFile("savedNotes.json", res.json(req.body), function(err) {
+        if (err) throw err;
+
+        console.log('Saved to json file');
+    });
+});
 
 
 
