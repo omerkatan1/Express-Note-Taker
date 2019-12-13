516b5db7d00ab36fe55ef0fbecfcafba6003cc85
Omer Katan <omerkatan@live.com>
2019-12-12 18:54:31 -0800

    folder stuff

--- public/js/server.js
+++ public/js/server.js
@@ -5,28 +5,33 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static('../../public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.get("/api/savedNotes", function (req, res) {
-    // TODO: retrieve reservations and send them back as JSON
-    console.log(JSON.stringify(res.json));
-});
 
-app.post("/api/savedNotes", function (req, res) {
-    // TODO: receive data and create reservation
-    // res.json(req.body);
-    fs.appendFile("savedNotes.json", res.json(req.body), function(err) {
+
+    fs.appendFile("savedNotes.JSON", JSON.parse(res.body), function (err) {
         if (err) throw err;
 
         console.log('Saved to json file');
     });
+
 });
 
+app.post("/api/savedNotes", function (req, res) {
+// TODO: receive data and create reservation
+res.json(req.body);
+// fs.appendFile("savedNotes.json", res.json(req.body), function(err) {
+//     if (err) throw err;
 
+//     console.log('Saved to json file');
+// });
+});
 
-app.use(express.static('../../public'));
 
 
 app.listen(PORT);
